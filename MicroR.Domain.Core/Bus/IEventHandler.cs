510fe67daaa634acdb9285a105978ab87fbd9f3b
Drew <drewharper29@gmail.com>
2021-02-21 17:07:07 -0500

    Implemented Interfaces and classes for Domain.Core

---
+++ MicroR.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using MicroR.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroR.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
---
+++ MicroR.Domain.Core/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using MicroR.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroR.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}

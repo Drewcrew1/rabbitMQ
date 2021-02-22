730a9ef7861e3d6e5c13be058d848e50b90440f9
Drew <drewharper29@gmail.com>
2021-02-22 18:09:35 -0500

    initial commit for infra dependency project

---
+++ MicroR.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MicroR.Infra.Bus;
+using MicroR.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroR.Infra.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}

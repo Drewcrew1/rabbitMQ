83200a37a8321971c2f587ff59109f8c16c8d2c2
Drew <drewharper29@gmail.com>
2021-02-27 08:56:02 -0500

    integrated event bus and service into post method to create trasnfer

---
+++ MiroR.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using MicroR.Banking.Domain.Commands;
+using MicroR.Banking.Domain.Events;
+using MicroR.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroR.Banking.Domain.CommandHandlers
+{
+    public class TransferCommandHandler : IRequestHandler<CreateTransferCommand, bool>
+    {
+        private readonly IEventBus _bus;
+
+        public TransferCommandHandler(IEventBus bus)
+        {
+            _bus = bus;
+        }
+
+        public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+        {
+
+            //public event to rabbitMQ
+            _bus.Publish(new TransferCreatedEvent(request.From, request.To, request.Amount));
+
+            return Task.FromResult(true);
+        }
+    }
+}

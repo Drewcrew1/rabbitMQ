5af11819e18adff27d145914d04b103e7a0322c5
Drew <drewharper29@gmail.com>
2021-02-27 13:48:05 -0500

    added mvc project for presentation testing of banking ai transfer

---
+++ MicroR.MVC/Models/Dto/TransferDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroR.MVC.Models.Dto
+{
+    public class TransferDto
+    {
+        public decimal TransferAmount { get; set; }
+        public int FromAccount { get; set; }
+        public int ToAccount { get; set; }
+    }
+}
---
+++ MicroR.MVC/Services/ITransferService.cs
@@ -0,0 +1,13 @@
+﻿using MicroR.MVC.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroR.MVC.Services
+{
+    public interface ITransferService
+    {
+        Task Transfer(TransferDto transferdto);
+    }
+}
